b2075747658f19dfa7cbec928eda20eb11e65a30
Megan Moore <megan.moore@futurice.com@Megans-MBP.lan>
2021-04-01 16:24:33 +0200

    added shapes that are maneuverable around the screen

--- src/Quilt.js
+++ src/Quilt.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Draggable from 'react-draggable';
+import {Shape} from './Shape.js';
 
 const quiltStyles = {
   "pillowcase": {width: '20vw' , height: '20vw'},
@@ -18,79 +18,9 @@ export class Quilt extends React.Component {
       height: quiltStyles[this.props.quiltType].height,
       border: '1px solid',
       padding: '5px',
-      activeDrags: 0,
-      deltaPosition: {
-        x: 0, y: 0
-      },
-      controlledPosition: {
-        x: -400, y: 200
-      }
     };
   };
 
-  handleDrag(e, ui) {
-    const {x, y} = this.state.deltaPosition;
-    this.setState({
-      deltaPosition: {
-        x: x + ui.deltaX,
-        y: y + ui.deltaY,
-      }
-    });
-  };
-
-  onStart() {
-    this.setState({
-      activeDrags: (this.state.activeDrags+1),
-    });
-  };
-
-  onStop() {
-    this.setState({activeDrags: (this.state.activeDrags - 1)});
-  };
-
-  onDrop(e) {
-    this.setState({activeDrags: (this.state.activeDrags - 1)});
-    if (e.target.classList.contains("drop-target")) {
-      alert("Dropped!");
-      e.target.classList.remove('hovered');
-    }
-  };
-
-  onDropAreaMouseEnter(e) {
-    if (this.state.activeDrags) {
-      e.target.classList.add('hovered');
-    };
-  };
-
-  onDropAreaMouseLeave(e) {
-    e.target.classList.remove('hovered');
-  };
-
-  // For controlled component
-  adjustXPos(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const {x, y} = this.state.controlledPosition;
-    this.setState({controlledPosition: {x: x - 10, y}});
-  };
-
-  adjustYPos(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const {controlledPosition} = this.state;
-    const {x, y} = controlledPosition;
-    this.setState({controlledPosition: {x, y: y - 10}});
-  };
-
-  onControlledDrag(e, position) {
-    const {x, y} = position;
-    this.setState({controlledPosition: {x, y}});
-  };
-
-  onControlledDragStop(e, position) {
-    this.onControlledDrag(e, position);
-    this.onStop();
-  };
 
   updateDims (type) {
     const newStyle = quiltStyles[type]
@@ -104,13 +34,9 @@ export class Quilt extends React.Component {
   };
 
   render() {
-    const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
-    const {deltaPosition, controlledPosition} = this.state;
     return(
       <div style={this.state}>
-        <Draggable grid={[25, 25]} {...dragHandlers}>
-            <div className="box">I snap to a 25 x 25 grid</div>
-        </Draggable>
+      <Shape text="blah" type="square"/>
       </div>
     );
   };
---
+++ src/Shape.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Moveable from 'react-moveable';
+
+export function Shape(props) {
+  const text = props.text;
+  const height = props.height;
+  const width = props.width;
+  const [target, setTarget] = React.useState();
+  const [frame, setFrame] = React.useState({
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  });
+
+  React.useEffect(() => {
+      setTarget(document.querySelector(".target-triangle"));
+  }, []);
+
+  return(
+    <div className="container">
+      <div className="target-triangle">{text}</div>
+      <Moveable
+          target={target}
+          resizable={true}
+          keepRatio={true}
+          throttleResize={0}
+          originDraggable={true}
+          originRelative={true}
+          draggable={true}
+          throttleDrag={0}
+          startDragRotate={0}
+          throttleDragRotate={0}
+          renderDirections={["nw","n","ne","w","e","sw","s","se"]}
+          edge={false}
+          zoom={1}
+          origin={false}
+          padding={{"left":0,"top":0,"right":0,"bottom":0}}
+          rotatable={true}
+          throttleRotate={0}
+          rotationPosition={"top"}
+          snappable={true}
+          verticalGuidelines={[0,200,400]} // adjust to scale of quilt
+          horizontalGuidelines={[0,200,400]}
+          snapThreshold={5}
+          isDisplaySnapDigit={true}
+          snapGap={true}
+          snapElement={true}
+          snapVertical={true}
+          snapHorizontal={true}
+          snapCenter={false}
+          snapDigit={0}
+          onResizeStart={e => {
+              e.setOrigin(["%", "%"]);
+              e.dragStart && e.dragStart.set(frame.translate);
+          }}
+          onResize={e => {
+              const beforeTranslate = e.drag.beforeTranslate;
+
+              frame.translate = beforeTranslate;
+              e.target.style.width = `${e.width}px`;
+              e.target.style.height = `${e.height}px`;
+              e.target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+          }}
+          onDragOriginStart={e => {
+                e.dragStart && e.dragStart.set(frame.translate);
+            }}
+          onDragOrigin={e => {
+              frame.translate = e.drag.beforeTranslate;
+              frame.transformOrigin = e.transformOrigin;
+          }}
+          onDragStart={e => {
+              e.set(frame.translate);
+          }}
+          onDrag={e => {
+              frame.translate = e.beforeTranslate;
+              e.target.style.transform = `translate(${e.beforeTranslate[0]}px, ${e.beforeTranslate[1]}px)`;
+          }}
+          onRotateStart={e => {
+                e.set(frame.rotate);
+            }}
+          onRotate={e => {
+              frame.rotate = e.beforeRotate;
+          }}
+          onRender={e => {
+              const { translate, rotate, transformOrigin } = frame;
+              e.target.style.transformOrigin = transformOrigin;
+              e.target.style.transform = `translate(${translate[0]}px, ${translate[1]}px)`
+                  +  ` rotate(${rotate}deg)`;
+          }}
+      />
+  </div>
+  );
+}
